2a09f3a5d256168cc28050fae21312f61648c6fd
chippymint <87289209+chippymint@users.noreply.github.com>
2023-05-31 20:27:14 -0400

    Add files via upload

---
+++ background removal.py
@@ -0,0 +1,56 @@
+# import cv2 to capture videofeed
+import cv2
+
+import numpy as np
+
+# attach camera indexed as 0
+camera = cv2.VideoCapture(0)
+
+# setting framewidth and frameheight as 640 X 480
+camera.set(3 , 640)
+camera.set(4 , 480)
+
+# loading the mountain image
+mountain = cv2.imread('mount everest.jpg')
+
+# resizing the mountain image as 640 X 480
+
+
+while True:
+
+    # read a frame from the attached camera
+    status , frame = camera.read()
+
+    # if we got the frame successfully
+    if status:
+
+        # flip it
+        frame = cv2.flip(frame , 1)
+
+        # converting the image to RGB for easy processing
+        frame_rgb = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
+
+        # creating thresholds
+        lower_bound = np.array([0, 120, 50])
+        upper_bound = np.array([10, 255, 255])
+
+        # thresholding image
+        mask_1 = cv2.inRange(hsv, lower_red, upper_red)
+
+        # bitwise and operation to extract foreground / person
+        red_detected = removePerson
+
+        # final image
+        final_output = img
+
+        # show it
+        cv2.imshow('frame' , frame)
+
+        # wait of 1ms before displaying another frame
+        code = cv2.waitKey(1)
+        if code  ==  32:
+            break
+
+# release the camera and close all opened windows
+camera.release()
+cv2.destroyAllWindows()
